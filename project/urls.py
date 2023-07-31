0206c0092def511b63b5724ca95537c08cc02d2a
dinajar <deenajaradat2@gmail.com>
2023-08-01 01:36:27 +0300

    update

--- project/urls.py
+++ project/urls.py
@@ -23,10 +23,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/cookie_stands/", include("cookie_stands.urls")),
     path("api-auth/", include("rest_framework.urls")),
-    path(
-        "api/token/",
-        MyTokenObtainPairView.as_view(),
-        name="token_obtain_pair",
+    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'
     ),
     path(
         "api/token/refresh/",
